e5c3b48e30309b1ad6c5cc867c20552f56a60fc2
Kyrylo Hliebov <kirilglebow030@gmail.com>
2023-04-30 20:26:32 +0300

    some fixes

--- js/main.js
+++ js/main.js
@@ -10,18 +10,6 @@ for (i = 0; i < acd.length; ++i) {
     };
 };
 
-////// 404 finder //////
-window.onbeforeunload = () => {
-    if (window.performance && window.performance.navigation.type === 1) {
-        //page is reloaded or navigated to a new page
-        return;
-    }
-
-    if(document.getElementsByTagName("html"[0].getAttribute("data-status-code") === "404")) {
-        window.location.href = "/pages/404.html";
-    }
-};
-
 ////// NavToggle //////
 let button = document.getElementsByClassName('button');
 let toggleItems = document.getElementsByClassName('toggleItems');
